89fb11537fd38fbe4e3d0e4d12b7e53402964cdf
Jerico Reynaldi <jerico.reynaldi@edi-indonesia.local>
2019-11-26 14:59:43 +0700

    mengembalika email mu menjadi email

--- src/views/view_2.vue
+++ src/views/view_2.vue
@@ -25,12 +25,13 @@
 			        		label-for="input-1"
 			        		description="We'll never share your email with anyone else."
 			      		>
+
 			      		<b-form-input
          					id="input-1"
           					v-model="form.email"
           					type="email"
           					required
-          					placeholder="Enter emailmu"
+          					placeholder="Enter email"
         				>
         				</b-form-input>
         				</b-form-group>
